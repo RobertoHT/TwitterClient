22430a0372997c2a568215554a2e2f7d1d282b43
Roberto Hernández Tamayo <hetr@praxis.com.mx>
2016-06-14 11:20:00 -0500

    Configuración inicial

---
+++ app/src/main/java/edu/galileo/com/twitterclient/TwitterClientApp.java
@@ -0,0 +1,24 @@
+package edu.galileo.com.twitterclient;
+
+import android.app.Application;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by praxis on 14/06/16.
+ */
+public class TwitterClientApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initFabric();
+    }
+
+    private void initFabric(){
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+}
